90be6e3b75ae8b5b3d7319233babdbe7c7809856
vjudge <gradonday@gmail.com>
2021-07-18 21:58:45 +0800

    add basic

---
+++ training/basic-grammars/basic-type.py
@@ -0,0 +1,38 @@
+# 整数，浮点数，复数
+print('------ 整数，浮点数，复数 ------')
+a = 123
+b = 3.14
+print('a = ', int(b))
+print('b = ', float(a))
+print('complex(a) = ',complex(a))
+print('complex(a, b) = ',complex(a, b))
+
+# 字符串
+print('------ 字符串 ------')
+str1 = 'Hello world!'
+print('str1: ', str1)
+print('str1[0]: ', str1[0])
+print('str1[6:11]: ', str1[6:11])
+print('str1[6:11]: ', str1[6:11])
+
+# 元组
+tuple1 = ()
+# 如果元组只有一个元素，那么这个元素后面要加逗号。否则将被认为是基础类型
+tuple2 = (12,)
+tuple3 = (1, 2, 3)
+tuple4 = (3.14, 5.96, 1897, 2020, 'a', 3+4j) 
+# 创建元组，可以没有括号，元素之间用逗号分隔开即可
+tuple5 = 4, 5, 6
+
+
+
+
+
+
+
+
+
+
+
+
+
