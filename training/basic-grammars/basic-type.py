48000bd3bbb4a513acc86cf16f821c309cd87cc2
vjudge <gradonday@gmail.com>
2021-07-22 22:03:27 +0800

    update basic-type

--- training/basic-grammars/basic-type.py
+++ training/basic-grammars/basic-type.py
@@ -4,8 +4,8 @@ a = 123
 b = 3.14
 print('a = ', int(b))
 print('b = ', float(a))
-print('complex(a) = ',complex(a))
-print('complex(a, b) = ',complex(a, b))
+print('complex(a) = ', complex(a))
+print('complex(a, b) = ', complex(a, b))
 
 # 字符串
 print('------ 字符串 ------')
@@ -20,19 +20,6 @@ tuple1 = ()
 # 如果元组只有一个元素，那么这个元素后面要加逗号。否则将被认为是基础类型
 tuple2 = (12,)
 tuple3 = (1, 2, 3)
-tuple4 = (3.14, 5.96, 1897, 2020, 'a', 3+4j) 
+tuple4 = (3.14, 5.96, 1897, 2020, 'a', 3 + 4j)
 # 创建元组，可以没有括号，元素之间用逗号分隔开即可
 tuple5 = 4, 5, 6
-
-
-
-
-
-
-
-
-
-
-
-
-
