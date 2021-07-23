4fa81c4497f3e154fb3c8a741afb8da439b00a25
vjudge <gradonday@gmail.com>
2021-07-23 22:23:09 +0800

    update basic-type

--- training/basic-grammars/basic-type.py
+++ training/basic-grammars/basic-type.py
@@ -9,17 +9,20 @@ print('complex(a, b) = ', complex(a, b))
 
 # 字符串
 print('------ 字符串 ------')
-str1 = 'Hello world!'
+str1 = 'hello world!'
 print('str1: ', str1)
 print('str1[0]: ', str1[0])
 print('str1[6:11]: ', str1[6:11])
 print('str1[6:11]: ', str1[6:11])
 
-# 元组
-tuple1 = ()
-# 如果元组只有一个元素，那么这个元素后面要加逗号。否则将被认为是基础类型
-tuple2 = (12,)
-tuple3 = (1, 2, 3)
-tuple4 = (3.14, 5.96, 1897, 2020, 'a', 3 + 4j)
-# 创建元组，可以没有括号，元素之间用逗号分隔开即可
-tuple5 = 4, 5, 6
+print('字符替换：', str1.replace('world', 'python'))
+print('首字符大写：', str1.title())
+print('连接字符串：', '-'.join(['111', '222', '333']))
+
+
+
+
+
+
+
+
