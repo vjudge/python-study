cc22b880ddf801b852b2dd57bd806c2ecc7f4523
vjudge <gradonday@gmail.com>
2021-08-04 22:00:35 +0800

    update except

--- training/basic-grammars/except.py
+++ training/basic-grammars/except.py
@@ -1,4 +1,4 @@
-# excep
+# except
 
 try:
     s = input('please enter two numbers separated by comma: ')
@@ -6,3 +6,48 @@ try:
     num2 = int(s.split(',')[1].strip())
 except (ValueError, IndexError) as err:
     print('Error: {}'.format(err))
+except:  # 与任意异常相匹配（包括系统异常等）
+    print('Other error')
+
+# 同上
+try:
+    s = input('please enter two numbers separated by comma: ')
+    num1 = int(s.split(',')[0].strip())
+    num2 = int(s.split(',')[1].strip())
+except ValueError as err:
+    print('ValueError: {}'.format(err))
+except IndexError as err:
+    print('IndexError: {}'.format(err))
+except Exception as err:  # Exception 是其他所有非系统异常的基类，能够匹配任意非系统异常
+    print('OtherError: {}'.format(err))
+
+
+# 自定义异常
+class MyInputError(Exception):
+    """Exception raised when there're errors in input"""
+    def __init__(self, value):  # 自定义异常类型的初始化
+        self.value = value
+
+    def __str__(self):  # 自定义异常类型的string表达形式
+        return ('{} is invalid input'.format(repr(self.value)))
+
+
+try:
+    raise MyInputError(1)  # 抛出MyInputError这个异常
+except MyInputError as err:
+    print('error: {}'.format(err))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
