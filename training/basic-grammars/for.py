f5877fad181e5253bc80c26ee33a47afecbceafd
vjudge <gradonday@gmail.com>
2021-08-04 22:00:47 +0800

    update for

--- training/basic-grammars/for.py
+++ training/basic-grammars/for.py
@@ -6,7 +6,22 @@ for i in range(1, 10):
         print('%d*%d=%d'%(j, i, j*i), end='\t')
     print()
 
+attributes = ['name', 'dob', 'gender']
+values = [
+    ['jason', '2000-01-01', 'male'],
+    ['mike', '1999-01-01', 'male'],
+    ['nancy', '2001-02-01', 'female']
+]
 
+rst = []
+for objs in values:
+    mid_rst = {}
+    for i in range(0, len(objs)):
+            mid_rst[attributes[i]] = objs[i]
+    rst.append(mid_rst)
+print('rst: ', rst)
+
+print('rst: ', [dict(zip(attributes, value)) for value in values])
 
 
 
