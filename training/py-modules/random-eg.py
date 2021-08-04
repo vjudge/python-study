47daa57639138cb05020808cbf3a68d5dbde73c9
vjudge <gradonday@gmail.com>
2021-08-04 21:58:20 +0800

    add random

--- training/py-modules/random-eg.py
+++ training/py-modules/random-eg.py
@@ -5,6 +5,8 @@ from random import uniform
 rd10 = [round(random(), 2) for _ in range(10)]
 print('rd10: ', rd10)
 
+print('randint: ', random.randint(1, 5))
+
 # 生成 10 个 0 到 10 的满足均匀分布的浮点数
 rdu10 = [round(uniform(0, 10), 2) for _ in range(10)]
 print('rdu10: ', rdu10)
