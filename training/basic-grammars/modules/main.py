320793eb3a933451d4900cebbc994451dfaa9422
vjudge <gradonday@gmail.com>
2021-08-04 22:02:20 +0800

    add main

---
+++ training/basic-grammars/modules/main.py
@@ -0,0 +1,14 @@
+from libs.class_lib import PrintStr
+from def_lib import *
+# sys.path.append("..") 表示将当前程序所在位置向上提了一级
+# 运行 sys.path.append("..") ，则可以改变当前 Python 解释器的位置。不过，固定一个确定路径对大型工程来说是非常必要的。
+
+print_str('from def_lib import *')
+hello_world()
+hello_python()
+
+ps_en = PrintStr('I is class')
+ps_en.ps()
+
+
+
